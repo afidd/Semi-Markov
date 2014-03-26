1c8f10617203b8befa55fd1ae0b53b55afe9892e
Andrew Dolgert <adolgert@cornell.edu>
2014-03-25 22:16:30 -0400

    ExplicitTransitions now rigorously hides its members once it is built. There are also comments and tidying before committing to the main repository. Need to check this on gcc before the rebase.

--- src/gspn/explicit_transitions.h
+++ src/gspn/explicit_transitions.h
@@ -46,26 +46,38 @@ public:
 
 
 /*! This is one way to provide a set of transitions for a GSPN.
+ *  ETState is the state of the system. PKey is a place key, the identifier
+ *  for the place. TKey is the identifier for the transition. Random
+ *  is the random number generator.
  */
-template<typename ETState,typename PKey, typename TKey, typename Random>
+template<typename ETState,typename PKey, typename TKey, typename ETRand>
 class ExplicitTransitions
 {
-  // The GSPN gets the marking type from the State.
   typedef typename ETState::Marking ETMarking;
   typedef PetriGraphType PetriGraph;
   typedef BiGraphCorrespondence<PKey,TKey,
     boost::graph_traits<PetriGraph>::vertex_descriptor> BiMap;
-  BiMap _bimap;
 
 public:
   typedef smv::LocalMarking<ETMarking> LocalMarking;
   typedef PKey PlaceKey;
   typedef TKey TransitionKey;
-  typedef Random RNG;
+  typedef ETRand RNG;
   typedef boost::graph_traits<PetriGraph>::vertex_descriptor place_type;
   typedef boost::graph_traits<PetriGraph>::vertex_descriptor transition_type;
+  // This gspn expects transitions to be of this base class.
+  // Derive from this base class to make transitions.
   typedef smv::ExplicitTransition<LocalMarking,ETState,RNG> Transition;
 
+private:
+  // The GSPN gets the marking type from the State.
+  BiMap _bimap;
+  std::map<transition_type,
+      std::unique_ptr<ExplicitTransition<LocalMarking,ETState,RNG>>>
+      transitions;
+  PetriGraph graph;
+
+public:
   ExplicitTransitions(size_t num_vertices) : graph(num_vertices) {}
 
   // These copy constructor and copy assignment operators are
@@ -100,12 +112,34 @@ public:
   }
 
 
-  std::map<transition_type,
-      std::unique_ptr<ExplicitTransition<LocalMarking,ETState,RNG>>>
-      transitions;
-  PetriGraph graph;
+  friend BuildGraph<ExplicitTransitions<ETState,PKey,TKey,ETRand>>;
 
-  friend BuildGraph<ExplicitTransitions<ETState,PKey,TKey,Random>>;
+  template<typename State, typename P, typename T, typename Random>
+  friend
+  std::vector<std::tuple<size_t,size_t,int>>
+  neighbors_of_transition(
+    ExplicitTransitions<State,P,T,Random>& et,
+    typename ExplicitTransitions<State,P,T,Random>::transition_type trans_id);
+
+  template<typename F, typename State, typename P, typename T, typename Random>
+  friend
+  void neighbors_of_places(
+    ExplicitTransitions<State,P,T,Random>& et,
+    const std::set<typename ExplicitTransitions<State,P,T,Random>::place_type>&
+    place_id, F func);
+
+  template<typename Transitions, typename... Args>
+  friend
+  std::pair<bool,std::unique_ptr<TransitionDistribution<typename Transitions::RNG>>>
+  enabled(const Transitions& et, typename Transitions::transition_type trans_id,
+    Args&&... args);
+
+  template<typename Transitions, typename... Args>
+  friend
+  void
+  fire(Transitions& et, typename Transitions::transition_type trans_id,
+    Args&&... args);
+
 };
 
 
@@ -141,7 +175,8 @@ neighbors_of_transition(
 template<typename F, typename State, typename P, typename T, typename Random>
 void neighbors_of_places(
   ExplicitTransitions<State,P,T,Random>& et,
-  const std::set<typename ExplicitTransitions<State,P,T,Random>::place_type>& place_id, F func)
+  const std::set<typename ExplicitTransitions<State,P,T,Random>::place_type>&
+  place_id, F func)
 {
   return neighbors_of_places(et.graph, place_id, func);
 }
